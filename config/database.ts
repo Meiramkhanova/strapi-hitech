23affd33bc3d8767bc6008a6633abf2b254dd4d1
bl1nkker <nurovich14@gmail.com>
2025-09-19 13:53:02 +0500

    ci: added Dockerfile and docker-compose file

--- config/database.ts
+++ config/database.ts
@@ -9,9 +9,7 @@ module.exports = ({ env }) => ({
       user: env("DATABASE_USERNAME"),
       password: env("DATABASE_PASSWORD"),
       schema: env("DATABASE_SCHEMA", "public"), // Not required
-      ssl: {
-        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
-      },
+      ssl: env("DATABASE_SSL", false),
     },
     debug: false,
   },
